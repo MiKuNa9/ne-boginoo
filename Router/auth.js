98b419cd90143b1979d7851890a6cd2e7906c6ec
22-hop2-2B-NandinErdne <22-hop2-nandinerdne@Mstars-Lab1-09s-iMac.local>
2023-01-30 16:43:00 +0800

    new-boginoo

--- Router/auth.js
+++ Router/auth.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const { register, login, getUser } = require("../Controller/auth");
 const { createLink, getLink, getOriginal } = require("../Controller/link");
-// const heyCheckMeYourTax = (req, res, next) => {
-//   if (req.body?.email == true) {
-//     next();
-//   } else {
-//     res.status(404).send("Ticket is not valid");
-//   }
-// };
+
 const router = express.Router();
 
 router.post("/enter", login);
